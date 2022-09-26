4455720fde2dc9457146f7b02bbe953f9b8bf1ce
Liliia Semenenko <forworkinua@gmail.com>
2022-09-26 13:56:44 +0200

    add basic meal app logic

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import CartContext from "../store/cart-context";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.totalAmount;
+  }, 0);
+
+  return (
+    <button type="button" className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{cartCtx.totalAmount}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
